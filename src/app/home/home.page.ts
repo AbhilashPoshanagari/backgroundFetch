002dddc37d56786f982a316902edc6d4ec0178a3
Abhilash Poshanagari <abhi.sree762@gmail.com>
2020-07-18 15:59:30 +0530

    initial Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
+import { NavController, Platform } from '@ionic/angular';
+
+import BackgroundFetch from "cordova-plugin-background-fetch";
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,33 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  state: boolean;
 
-  constructor() {}
+  constructor( public platform: Platform,
+          private ngZone: NgZone ) {
+    this.state = false;
+    this.platform.ready().then(this.onDeviceReady.bind(this));
+  }
+  onDeviceReady() {
+    // Your background-fetch handler.
+    let fetchCallback = function(taskId) {
+        console.log('[js] BackgroundFetch event received from tgg-smartwatch : ', taskId);
+        // Required: Signal completion of your task to native code
+        // If you fail to do this, the OS can terminate your app
+        // or assign battery-blame for consuming too much background-time
+        this.ngZone.run(() => {
+          this.state = true;
+        })
+        BackgroundFetch.finish(taskId);
+    };
 
+    let failureCallback = function(error) {
+        console.log('- BackgroundFetch failed', error);
+    };
+
+    BackgroundFetch.configure(fetchCallback, failureCallback, {
+        minimumFetchInterval: 15, // <-- default is 15
+        stopOnTerminate: false
+    });
+  }
 }
